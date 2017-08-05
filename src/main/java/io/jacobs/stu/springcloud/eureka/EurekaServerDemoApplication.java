ac478672a40b025a8f664e2cfc378cc0ec27cb25
Lei Jun <leijun@lifang.com>
2017-08-05 22:23:04 +0800

    update eureka ha support demo

--- src/main/java/io/jacobs/stu/springcloud/eureka/EurekaServerDemoApplication.java
+++ src/main/java/io/jacobs/stu/springcloud/eureka/EurekaServerDemoApplication.java
@@ -1,12 +1,18 @@
 package io.jacobs.stu.springcloud.eureka;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerDemoApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(EurekaServerDemoApplication.class, args);
+		Logger.getLogger(EurekaServerDemoApplication.class)
+				.info("注册中心启动了。。。。");
 	}
 }
